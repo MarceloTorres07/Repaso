87c1935f729d6ddb1aa381a0d933ef9e44f41c0d
Marcelo Torres <marceloignacio.torres@alumnos.ulagos.cl>
2024-08-19 22:11:14 -0400

    repaso
    ver. 1.0.0

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+let Nombre="Marcelo Torres";
+console.log(Nombre);
+
+let Edad="29 años";
+console.log(Edad);
+
+
+let Numero=50;
+let Cadena = "hola a todo el mundo";
+let True = true;
+
+console.log(typeof Numero);
+console.log(typeof Cadena);
+console.log(typeof True);
+
+
+
+function suma(a, b) {
+    return a + b;
+}
+console.log(suma(4, 8));
+
+const resultado=suma(4, 8);
+console.log(resultado);
+
+function resta(a, b){
+    return a - b;
+}
+const resultadodos=resta(4, 8);
+console.log(resultadodos);
+
+
+
+let lista=[1,2,3,4,5]
+let listaeliminada=lista.pop(4);
+console.log(lista);
+console.log(listaeliminada);
+
+
+
+let libro={
+    titulo: 'el señor de los anillos ',
+    autor: 'J. R. R. Tolkien',
+
+};
+console.log(libro);
